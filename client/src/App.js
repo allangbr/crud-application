c598a38d5a0044361e9408a0bdfb83fcc3807173
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2022-04-28 11:17:03 -0300

    Interface de Cadastro Criada
    closes #6

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app-container'>
+      <div className='register-container'>
+        <h1 className='register-title'>Crud Games</h1>
+        <input type='text' name='name' placeholder=' Nome:' className='register-input'></input>
+        <input type='text' name='cost' placeholder=' Preço:' className='register-input'></input>
+        <input type='text' name='category' placeholder=' Categoria:' className='register-input'></input>
+        <button className='register-button'>Cadastrar</button>
+      </div>
     </div>
   );
 }
