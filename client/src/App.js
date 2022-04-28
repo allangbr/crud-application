77e1af0908a5591c97e98d3d7e72a7f66d1e36ca
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2022-04-28 18:50:10 -0300

    Cadastro com front/backend
    closes #7

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
+import React, { useState} from 'react'
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [values, setValues] = useState();
+
+  const handleClickButton = async () => {
+    Axios.post("http://localhost:3001/register", {
+      name: values.name,
+      cost: values.cost,
+      category: values.category,
+    }).then((response) => {
+      console.log(response);
+    })
+  }
+
+  const handleChangeValues = value => {
+    setValues(prevValue=>({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }))
+  }
+
   return (
     <div className='app-container'>
       <div className='register-container'>
         <h1 className='register-title'>Crud Games</h1>
-        <input type='text' name='name' placeholder=' Nome:' className='register-input'></input>
-        <input type='text' name='cost' placeholder=' Preço:' className='register-input'></input>
-        <input type='text' name='category' placeholder=' Categoria:' className='register-input'></input>
-        <button className='register-button'>Cadastrar</button>
+        <input type='text' name='name' placeholder=' Nome:' className='register-input' onChange={handleChangeValues}></input>
+        <input type='text' name='cost' placeholder=' Preço:' className='register-input' onChange={handleChangeValues}></input>
+        <input type='text' name='category' placeholder=' Categoria:' className='register-input' onChange={handleChangeValues}></input>
+        <button className='register-button' onClick={() => handleClickButton()}>Cadastrar</button>
       </div>
     </div>
   );
